63e3b1eb7e28d41ca09b13819dc3ccbe95e6dce1
aravindh203 <aravindhkumar218@gmail.com>
2023-01-09 17:45:59 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,78 @@
+
+                /*1*/
+function myfunction1(a,b){
+    c=a+b;
+    return c;
+}
+console.log(myfunction1(5,5));
+
+
+                /*2*/
+function myfunction2(a,b){
+    c=a*b;
+    return c;
+}
+console.log(myfunction2(5,5));
+
+                /*3*/
+function myfunction3(a,b){
+    c=a-b;
+    return c;
+}
+console.log(myfunction3(10,5));
+
+
+                /*4*/
+function myfunction4(a,b){
+    c=a%b;
+    return c;
+}
+console.log(myfunction4(10,5));
+
+
+                /*5*/
+function myfunction5(a,b){
+    c=a/b;
+    return c;
+}
+console.log(myfunction5(10,5));
+
+
+                /*6*/
+function myfunction6(a,b,c){
+    d=a+b+c;
+    return d;
+}
+console.log(myfunction6(5,5,5));
+
+
+                /*7*/
+function myfunction7(a,b,c){
+    d=a-b-c;
+    return d;
+}
+console.log(myfunction7(15,10,5));
+
+
+                /*8*/
+function myfunction8(a,b,c){
+    d=a*b*c;
+    return d;
+}
+console.log(myfunction8(5,5,5));
+
+                /*9*/
+function myfunction9(a,b,c){
+    d=a/b/c;
+    return d;
+}
+console.log(myfunction9(100,2,5));
+
+
+                /*10*/
+function myfunction10(a,b,c){
+    d=a/b%c;
+    return d;
+}
+console.log(myfunction10(100,2,5));
+
